e80a24e567ab36c4fd62fef18adacad09098b56b
Victor Bravim <victorrafaelbravim@gmail.com>
2024-07-17 17:58:29 -0300

    Add

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -5,28 +5,25 @@ const About: FC = () => {
     <div className="about bg-black text-white p-4">
 
       <div className="flex flex-col lg:flex-row gap-8">
-        <div className="w-full lg:w-[25%] flex justify-center bg-dark py-32 lg:py-72 rounded-[50px]">
-          <img src="app/assets/card1.png" alt="Card1" className="w-full h-auto" />
-          <h2 className="text-2xl font-bold mt-4">Card 1 (30%)</h2>
-          <p className="mt-4">Conteúdo do card 1.</p>
+        <div className="w-full lg:w-[30%] flex justify-center bg-dark rounded-[60px]">
+          <img src="app/assets/Card1.png" alt="Card1" className="w-full h-auto usernone" />
         </div>
-        <div className="w-full lg:w-[75%] flex justify-center bg-dark py-32 lg:py-72 rounded-[50px]">
-          <h2 className="text-2xl font-bold">Card 2 (70%)</h2>
-          <p className="mt-4">Conteúdo do card 2.</p>
+        <div className="w-full lg:w-[70%] flex justify-center bg-dark rounded-[60px]">
+          <img src="app/assets/Card2.png" alt="Card1" className="w-full h-auto usernone" />
         </div>
       </div>
 
       <div className="flex flex-col lg:flex-row gap-8 mt-8">
-        <div className="w-full lg:w-[50%] bg-dark p-32 lg:p-80 rounded-[50px]">
+        <div className="w-full lg:w-[50%] bg-dark p-32 lg:p-80 rounded-[60px]">
           <h2 className="text-2xl font-bold">Card 3 (50%)</h2>
           <p className="mt-4">Conteúdo do card 3.</p>
         </div>
         <div className="w-full lg:w-[50%] flex flex-col gap-8">
-          <div className="bg-dark py-32 lg:py-64 rounded-[50px]">
+          <div className="bg-dark py-32 lg:py-64 rounded-[60px]">
             <h2 className="text-2xl font-bold">Card 4</h2>
             <p className="mt-4">Conteúdo do card 4.</p>
           </div>
-          <button className="bg-dark py-32 lg:py-24 rounded-[50px]">
+          <button className="bg-dark py-32 lg:py-24 rounded-[60px]">
             <h2 className="text-2xl font-bold">Card 5</h2>
             <p className="mt-4">Conteúdo do card 5.</p>
           </button>
@@ -34,19 +31,19 @@ const About: FC = () => {
       </div>
 
       <div className="flex flex-col lg:flex-row gap-8 mt-8">
-        <div className="w-full lg:w-[25%] bg-dark p-32 lg:p-40 rounded-[50px]">
+        <div className="w-full lg:w-[25%] bg-dark p-32 lg:p-40 rounded-[60px]">
           <h2 className="text-2xl font-bold">Card 6</h2>
           <p className="mt-4">Conteúdo do card 6.</p>
         </div>
-        <div className="w-full lg:w-[25%] bg-dark p-32 lg:p-40 rounded-[50px]">
+        <div className="w-full lg:w-[25%] bg-dark p-32 lg:p-40 rounded-[60px]">
           <h2 className="text-2xl font-bold">Card 7</h2>
           <p className="mt-4">Conteúdo do card 7.</p>
         </div>
-        <div className="w-full lg:w-[25%] bg-dark p-32 lg:p-40 rounded-[50px]">
+        <div className="w-full lg:w-[25%] bg-dark p-32 lg:p-40 rounded-[60px]">
           <h2 className="text-2xl font-bold">Card 8</h2>
           <p className="mt-4">Conteúdo do card 8.</p>
         </div>
-        <div className="w-full lg:w-[25%] bg-dark p-32 lg:p-40 rounded-[50px]">
+        <div className="w-full lg:w-[25%] bg-dark p-32 lg:p-40 rounded-[60px]">
           <h2 className="text-2xl font-bold">Card 9</h2>
           <p className="mt-4">Conteúdo do card 9.</p>
         </div>
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero: FC = () => {
             <div className="absolute top-0 left-0 w-full">
                 <nav className="flex justify-between items-center p-12 bg-transparent">
                     <div className="hidden lg:flex rounded-full">
-                        <img src="app/assets/Logo.png" alt="Logo" className="w-24 h-24 object-contain"/>
+                        <img src="app/assets/Logo.png" alt="Logo" className="w-24 h-24 object-contain usernone"/>
                     </div>
                     <div className="w-full flex justify-between items-center space-x-0 lg:space-x-12 bg-gradient-to-r from-white to-blue p-7 rounded-full lg:mx-6">
                         <div className="flex space-x-12">
@@ -26,7 +26,7 @@ const Hero: FC = () => {
             <div className="w-full h-screen flex items-center justify-center p-4">
                 <div className="bg-dark flex flex-row items-center justify-center rounded-[50px] w-full h-full p-4">
                     <div className="bg-dark flex items-end w-[100%] h-full p-12">
-                        <img src="app/assets/Hero.png" alt="image" className="w-full h-[666px]" />
+                        <img src="app/assets/Hero.png" alt="image" className="w-full h-[666px] usernone" />
                     </div>
                     <div className="bg-dark flex justify-end w-[25%] h-full items-end">
                         <button className="px-9 py-11 bg-white text-dark font-bold rounded-full">
