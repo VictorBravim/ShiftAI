5afd8ca05d48a6fe6b12867308a68c5274a299de
Victor Bravim <victorrafaelbravim@gmail.com>
2024-07-17 18:16:21 -0300

    Add

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -9,41 +9,37 @@ const About: FC = () => {
           <img src="app/assets/Card1.png" alt="Card1" className="w-full h-auto usernone" />
         </div>
         <div className="w-full lg:w-[70%] flex justify-center bg-dark rounded-[60px]">
-          <img src="app/assets/Card2.png" alt="Card1" className="w-full h-auto usernone" />
+          <img src="app/assets/Card2.png" alt="Card2" className="w-full h-auto usernone" />
         </div>
       </div>
 
       <div className="flex flex-col lg:flex-row gap-8 mt-8">
-        <button className="w-full lg:w-[50%] bg-dark rounded-[65px]">
-        <img src="app/assets/Card3.png" alt="Card1" className="w-full h-auto usernone" />
-        </button>
+        <div className="w-full lg:w-[50%] bg-dark rounded-[65px]">
+        <img src="app/assets/Card3.png" alt="Card3" className="w-full h-auto usernone" />
+        </div>
         <div className="w-full lg:w-[50%] flex flex-col gap-8">
-          <button className="bg-dark rounded-[60px]">
-          <img src="app/assets/Card4.png" alt="Card1" className="w-full h-auto usernone" />
-          </button>
-          <button className="bg-dark rounded-[60px]">
-          <img src="app/assets/Card5.png" alt="Card1" className="w-full h-auto usernone" />
-          </button>
+          <div className="bg-dark rounded-[60px]">
+          <img src="app/assets/Card4.png" alt="Card4" className="w-full h-auto usernone" />
+          </div>
+          <div className="bg-dark rounded-[60px]">
+          <img src="app/assets/Card5.png" alt="Card5" className="w-full h-auto usernone" />
+          </div>
         </div>
       </div>
 
       <div className="flex flex-col lg:flex-row gap-8 mt-8">
-        <div className="w-full lg:w-[25%] bg-dark p-32 lg:p-40 rounded-[60px]">
-          <h2 className="text-2xl font-bold">Card 6</h2>
-          <p className="mt-4">Conteúdo do card 6.</p>
-        </div>
-        <div className="w-full lg:w-[25%] bg-dark p-32 lg:p-40 rounded-[60px]">
-          <h2 className="text-2xl font-bold">Card 7</h2>
-          <p className="mt-4">Conteúdo do card 7.</p>
-        </div>
-        <div className="w-full lg:w-[25%] bg-dark p-32 lg:p-40 rounded-[60px]">
-          <h2 className="text-2xl font-bold">Card 8</h2>
-          <p className="mt-4">Conteúdo do card 8.</p>
-        </div>
-        <div className="w-full lg:w-[25%] bg-dark p-32 lg:p-40 rounded-[60px]">
-          <h2 className="text-2xl font-bold">Card 9</h2>
-          <p className="mt-4">Conteúdo do card 9.</p>
-        </div>
+        <button className="w-full lg:w-[25%] bg-dark rounded-[60px]">
+        <img src="app/assets/Card6.png" alt="Card6" className="w-full h-auto usernone" />
+        </button>
+        <button className="w-full lg:w-[25%] bg-dark rounded-[60px]">
+        <img src="app/assets/Card7.png" alt="Card7" className="w-full h-auto usernone" />
+        </button>
+        <button className="w-full lg:w-[25%] bg-dark rounded-[60px]">
+        <img src="app/assets/Card8.png" alt="Card8" className="w-full h-auto usernone" />
+        </button>
+        <button className="w-full lg:w-[25%] bg-dark rounded-[60px]">
+        <img src="app/assets/Card9.png" alt="Card9" className="w-full h-auto usernone" />
+        </button>
       </div>
 
     </div>
